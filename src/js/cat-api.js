ae3384e1b52296b4c11a5e6f4ba2b82c77d1318c
Yurchyk Yurii <124770215+yurchykyur@users.noreply.github.com>
2023-05-28 13:13:54 +0000

    it remains to deal with the error notification

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,30 +1,29 @@
 export function fetchBreeds() {
-    console.log(
-      fetch('https://api.thecatapi.com/v1/breeds').then(response =>
-        response.json()
-      )
-    );
-    return fetch('https://api.thecatapi.com/v1/breeds').then(response =>
-      response.json()
-    );
-  }
-  
-  const API_KEY =
-    'live_wkn7NG9gg6cEVzYeEh1vrLo4JBR3UguOoVTAaAJh8My7YvuoXdZc8OMPTlaH22iL';
-  
-  export function fetchCatByBreed(breedId) {
-    return fetch(`https://api.thecatapi.com/v1/breeds/${breedId}`, {
-      headers: {
-        'x-api-key': API_KEY,
-      },
-    }).then(response => response.json());
-  }
-  
-  export function fetchImageCatByBreed(imageId) {
-    return fetch(`https://api.thecatapi.com/v1/images/${imageId}`, {
-      headers: {
-        'x-api-key': API_KEY,
-      },
-    }).then(response => response.json());
-  }
-  
+  // console.log(
+  //   fetch('https://api.thecatapi.com/v1/breeds').then(response =>
+  //     response.json()
+  //   )
+  // );
+  return fetch('https://api.thecatapi.com/v1/breeds', options).then(response =>
+    response.json()
+  );
+}
+
+const options = {
+  headers: {
+    'x-api-key':
+      'live_wkn7NG9gg6cEVzYeEh1vrLo4JBR3UguOoVTAaAJh8My7YvuoXdZc8OMPTlaH22iL',
+  },
+};
+
+export function fetchCatByBreed(breedId) {
+  return fetch(`https://api.thecatapi.com/v1/breeds/${breedId}`, options).then(
+    response => response.json()
+  );
+}
+
+export function fetchImageCatByBreed(imageId) {
+  return fetch(`https://api.thecatapi.com/v1/images/${imageId}`, options).then(
+    response => response.json()
+  );
+}
